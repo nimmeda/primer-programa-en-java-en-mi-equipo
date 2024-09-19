6bbe018eabaa57b994ef8efedfad2d4ef6a566d0
Koke <jmediavilla@gmail.com>
2024-09-19 10:02:16 +0200

    Enseña por consola mis apellidos, edad, fecha de nacimieno y comida favorita

--- Prueba.java
+++ Prueba.java
@@ -3,5 +3,6 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("Mediavilla Aller");
         System.out.println("19 años, 05/08/2005");
+        System.out.println("Arroz con pollo");
     }
 }

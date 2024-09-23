54f27a0b9ebdfd6f5716d6eafe51fa83be3cb993
Koke <jmediavilla@gmail.com>
2024-09-23 12:42:42 +0200

    añado mi cantante favorito y elimino mis apellidos

--- Prueba.java
+++ Prueba.java
@@ -1,8 +1,9 @@
 public class Prueba {
 
     public static void main(String[] args) {
-        System.out.println("Mediavilla Aller");
+        // System.out.println("Mediavilla Aller");
         System.out.println("19 años, 05/08/2005");
         System.out.println("Arroz con pollo");
+        System.out.println("Mi cantante favorito es Leïti");
     }
 }
